686646c20bd7dbed5ab6eee23adc8c2ef6a1b670
Zwx <staszekd2020@gmail.com>
2024-01-30 18:47:24 +0100

    task end date, final twix

--- dialog.py
+++ dialog.py
@@ -1,10 +1,12 @@
 import customtkinter as ctk
-from typing import Union, Tuple, Optional
+from typing import Optional, Union, Tuple
 
 class CustomDialog(ctk.CTkInputDialog):
 
     def __init__(self,
-                 text_value: Optional[str] = "",  # New parameter
+                 text: Optional[str] = "",
+                 end_date: Optional[str] = "",
+                 hour: Optional[str] = "",
                  fg_color: Optional[Union[str, Tuple[str, str]]] = None,
                  text_color: Optional[Union[str, Tuple[str, str]]] = None,
                  button_fg_color: Optional[Union[str, Tuple[str, str]]] = None,
@@ -14,12 +16,16 @@ class CustomDialog(ctk.CTkInputDialog):
                  entry_border_color: Optional[Union[str, Tuple[str, str]]] = None,
                  entry_text_color: Optional[Union[str, Tuple[str, str]]] = None,
                  title: str = "CTkDialog",
-                 font: Optional[Union[tuple, ctk.CTkFont]] = None,
-                 text: str = "CTkDialog"):
+                 font: Optional[Union[Tuple[str, int], ctk.CTkFont]] = None,
+                 text_value: Optional[str] = ""
+        
+        ):
 
         super().__init__(fg_color, text_color, button_fg_color, button_hover_color, button_text_color,
                          entry_fg_color, entry_border_color, entry_text_color, title, font, text)
 
+        self._end_date = end_date
+        self._hour = hour
         self._text_value = text_value
 
     def _create_widgets(self):
@@ -27,48 +33,96 @@ class CustomDialog(ctk.CTkInputDialog):
         self.rowconfigure(0, weight=1)
 
         self._label = ctk.CTkLabel(master=self,
-                               width=300,
-                               wraplength=300,
-                               fg_color="transparent",
-                               text_color=self._text_color,
-                               text=self._text,
-                               font=self._font)
+                                   width=300,
+                                   wraplength=300,
+                                   fg_color="transparent",
+                                   text_color=self._text_color,
+                                   text=self._text,
+                                   font=self._font)
         self._label.grid(row=0, column=0, columnspan=2, padx=20, pady=20, sticky="ew")
 
+        # Entry for the main text
+        self._entry_label = ctk.CTkLabel(master=self,
+                                         text="Task Description:",
+                                         fg_color="transparent",
+                                         text_color=self._text_color,
+                                         font=self._font)
+        self._entry_label.grid(row=1, column=0, columnspan=2, padx=20, pady=(0, 10), sticky="w")
         self._entry = ctk.CTkEntry(master=self,
-                               width=230,
-                               fg_color=self._entry_fg_color,
-                               border_color=self._entry_border_color,
-                               text_color=self._entry_text_color,
-                               font=self._font)
-        
+                                   width=230,
+                                   fg_color=self._entry_fg_color,
+                                   border_color=self._entry_border_color,
+                                   text_color=self._entry_text_color,
+                                   font=self._font)
         self._entry.insert(0, self._text_value)
-        self._entry.grid(row=1, column=0, columnspan=2, padx=20, pady=(0, 20), sticky="ew")
-        self._ok_button = ctk.CTkButton(master=self,
-                                    width=100,
-                                    border_width=0,
-                                    fg_color=self._button_fg_color,
-                                    hover_color=self._button_hover_color,
-                                    text_color=self._button_text_color,
-                                    text='Ok',
-                                    font=self._font,
-                                    command=self._ok_event)
-        self._ok_button.grid(row=2, column=0, columnspan=1, padx=(20, 10), pady=(0, 20), sticky="ew")
+        self._entry.grid(row=2, column=0, columnspan=2, padx=20, pady=(0, 10), sticky="ew")
 
-        self._cancel_button = ctk.CTkButton(master=self,
+        # Entry for the end date
+        self._end_date_label = ctk.CTkLabel(master=self,
+                                            text="End Date(DD-MM-RRRR):",
+                                            fg_color="transparent",
+                                            text_color=self._text_color,
+                                            font=self._font)
+        self._end_date_label.grid(row=3, column=0, padx=20, pady=(0, 10), sticky="w")
+
+        self._end_date_entry = ctk.CTkEntry(master=self,
+                                            width=230,
+                                            fg_color=self._entry_fg_color,
+                                            border_color=self._entry_border_color,
+                                            text_color=self._entry_text_color,
+                                            font=self._font)
+        self._end_date_entry.insert(0, self._end_date)
+        self._end_date_entry.grid(row=4, column=0, padx=20, pady=(0, 10), sticky="ew", columnspan=2)
+        
+        # Entry for the hour
+        self._hour_label = ctk.CTkLabel(master=self,
+                                        text="Hour(HH:MM):",
+                                        fg_color="transparent",
+                                        text_color=self._text_color,
+                                        font=self._font)
+        self._hour_label.grid(row=5, column=0, padx=20, pady=(0, 20), sticky="w", columnspan=2)
+
+        self._hour_entry = ctk.CTkEntry(master=self,
+                                        width=230,
+                                        fg_color=self._entry_fg_color,
+                                        border_color=self._entry_border_color,
+                                        text_color=self._entry_text_color,
+                                        font=self._font)
+        self._hour_entry.insert(0, self._hour)
+        self._hour_entry.grid(row=6, column=0, padx=20, pady=(0, 20), sticky="ew", columnspan=2)
+
+        self._ok_button = ctk.CTkButton(master=self,
                                         width=100,
                                         border_width=0,
                                         fg_color=self._button_fg_color,
                                         hover_color=self._button_hover_color,
                                         text_color=self._button_text_color,
-                                        text='Cancel',
+                                        text='Ok',
                                         font=self._font,
-                                        command=self._cancel_event)
-        self._cancel_button.grid(row=2, column=1, columnspan=1, padx=(10, 20), pady=(0, 20), sticky="ew")
+                                        command=self._ok_event)
+        self._ok_button.grid(row=7, column=0, columnspan=1, padx=(20, 10), pady=(0, 20), sticky="ew")
 
-        self.after(150, lambda: self._entry.focus())  # set focus to entry with slight delay, otherwise it won't work
+        self._cancel_button = ctk.CTkButton(master=self,
+                                            width=100,
+                                            border_width=0,
+                                            fg_color=self._button_fg_color,
+                                            hover_color=self._button_hover_color,
+                                            text_color=self._button_text_color,
+                                            text='Cancel',
+                                            font=self._font,
+                                            command=self._cancel_event)
+        self._cancel_button.grid(row=7, column=1, columnspan=1, padx=(10, 20), pady=(0, 20), sticky="ew")
+
+        self.after(150, lambda: self._entry.focus())
         self._entry.bind("<Return>", self._ok_event)
 
-    def set_entry(self, text):
-        print(self)
-        # self._entry.insert(0, text)
+    def _ok_event(self, event=None):
+        self._user_input = self._entry.get()
+        self._end_date_input = self._end_date_entry.get()
+        self._hour_input = self._hour_entry.get()
+        self.grab_release()
+        self.destroy()
+
+    def get_input(self):
+        self.master.wait_window(self)
+        return self._user_input, self._end_date_input, self._hour_input
--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import customtkinter as ctk
 from dialog import CustomDialog
 
 class ToDoElement(ctk.CTkFrame):
-    def __init__(self, master, text, delete_callback, edit_callback, **kwargs):
+
+    def __init__(self, master, text, checked, end_date, end_hour, delete_callback, edit_callback, update_callback, **kwargs):
         super().__init__(master, **kwargs)
-        self.checked_var = ctk.BooleanVar(value=False)
+
+        self.end_date = end_date
+        self.end_hour = end_hour
+
+        self.checked_var = ctk.BooleanVar(value=checked)
         self.checkbox = ctk.CTkCheckBox(self, height=20, width=25, text="", variable=self.checked_var, command=self.checkbox_event)
         self.checkbox.pack(side='left', pady="5")
         self.label = ctk.CTkLabel(self, height=20, text=text, font=("Arial", 12))
-        self.label.pack(side='left')
+        self.label.pack(side='left', fill="x", expand=True)
+        
+        self.end_date_label = ctk.CTkLabel(self, height=20, text=f"End Date: {end_date} {end_hour}", font=("Arial", 10))
+        self.end_date_label.pack(side='left', fill="x", expand=True)
         self.edit_button = ctk.CTkButton(self, height=30, width=50, text="Edit", command=lambda: edit_callback(self))
         self.edit_button.pack(side='right', padx=(10, 0))
         self.delete_button = ctk.CTkButton(self, height=30, width=50, text="Delete", command=lambda: delete_callback(self))
         self.delete_button.pack(side='right')
-
+        self.update_callback = update_callback
+    
     def checkbox_event(self):
-        print(f"Checkbox toggled for '{self.label.cget('text')}', current value: {self.checked_var.get()}")
+        self.update_callback()
 
 class MyFrame(ctk.CTkScrollableFrame):
     def __init__(self, master, db_connection, **kwargs):
@@ -26,15 +35,18 @@ class MyFrame(ctk.CTkScrollableFrame):
 
     def load_tasks(self):
         cursor = self.db_connection.cursor()
-        cursor.execute("SELECT task_text FROM tasks")
+        cursor.execute("SELECT task_text, checked, end_date, end_hour FROM tasks")
         rows = cursor.fetchall()
-        tasks = [row[0] for row in rows]
         todo_elements = []
-        for index, item in enumerate(tasks):
-            todo_element = ToDoElement(self, text=item, delete_callback=self.delete_task, edit_callback=self.edit_task)
-            todo_element.grid(row=index, column=0, padx=20, pady=5, sticky="ew")
+        for index, (item, checked, end_date, end_hour) in enumerate(rows):
+            todo_element = ToDoElement(
+                self, text=item, checked=checked, end_date=end_date, end_hour=end_hour,
+                update_callback=self.update_database, delete_callback=self.delete_task, edit_callback=self.edit_task
+            )
+            todo_element.pack(side="top", fill="x", padx="5", pady="5")
             todo_elements.append(todo_element)
         return todo_elements
+
 
     def delete_task(self, todo_element):
         self.todo_elements.remove(todo_element)
@@ -42,19 +54,29 @@ class MyFrame(ctk.CTkScrollableFrame):
         self.update_database()
 
     def edit_task(self, todo_element):
-        # Open a dialog for editing the task
-        edit_dialog = CustomDialog(text="Edit task description:", title="Edit Task", text_value=todo_element.label.cget('text'))
-        edited_text = edit_dialog.get_input()
+        task_time = todo_element.end_date_label.cget('text').split(" ")
+
+        edit_dialog = CustomDialog(text="Edit task description:", title="Edit Task", text_value=todo_element.label.cget('text'), end_date=task_time[2], hour=task_time[3])
+        edited_text, edited_date, edited_hour = edit_dialog.get_input()
         if edited_text:
             todo_element.label.configure(text=edited_text)
+            todo_element.end_date_label.configure(text=f"End Date: {edited_date} {edited_hour}")
+            todo_element.end_date = edited_date
+            todo_element.end_hour = edited_hour
             self.update_database()
+
 
     def update_database(self):
         cursor = self.db_connection.cursor()
         cursor.execute("DELETE FROM tasks")
         for task_element in self.todo_elements:
-            cursor.execute("INSERT INTO tasks (task_text) VALUES (?)", (task_element.label.cget('text'),))
-        self.db_connection.commit()
+            cursor.execute(
+                "INSERT INTO tasks (task_text, checked, end_date, end_hour) VALUES (?, ?, ?, ?)",
+                (task_element.label.cget('text'), task_element.checked_var.get(), task_element.end_date, task_element.end_hour)
+            )
+        self.db_connection.commit() 
+
+
 
 class App(ctk.CTk):
     def __init__(self, db_connection):
@@ -62,32 +84,33 @@ class App(ctk.CTk):
         self.db_connection = db_connection
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.my_frame = MyFrame(master=self, db_connection=self.db_connection, width=300, height=200, corner_radius=0, fg_color="transparent")
+        self.my_frame = MyFrame(master=self, db_connection=self.db_connection, width=450, height=200, corner_radius=0, fg_color="transparent")
         self.my_frame.grid(row=0, column=0, sticky="nsew", pady=5)
         self.add_button = ctk.CTkButton(self, text="Add Task", command=self.add_task)
         self.add_button.grid(row=1, column=0, pady=10)
 
     def add_task(self):
-        task_dialog = ctk.CTkInputDialog(text="Type task description:", title="Add new task")
-        new_task_text = task_dialog.get_input()
+        new_task_dialog = CustomDialog(text="Edit task description:", title="Edit Task")
+        new_task_text, new_task_date, new_task_hour = new_task_dialog.get_input()
         if new_task_text:
-            new_todo_element = ToDoElement(self.my_frame, text=new_task_text, delete_callback=self.my_frame.delete_task, edit_callback=self.my_frame.edit_task)
-            new_todo_element.grid(row=len(self.my_frame.todo_elements), column=0, padx=20, pady=5, sticky="ew")
+            new_todo_element = ToDoElement(self.my_frame, text=new_task_text, update_callback=self.my_frame.update_database, delete_callback=self.my_frame.delete_task, edit_callback=self.my_frame.edit_task, checked=False, end_date=new_task_date, end_hour=new_task_hour)
+            new_todo_element.pack(side="top", fill="x", padx="5", pady="5")
             self.my_frame.todo_elements.append(new_todo_element)
             self.my_frame.update_database()
 
-# Connect to SQLite database
 conn = sqlite3.connect('tasks.db')
 
-# Create tasks table if not exists
 conn.execute('''
     CREATE TABLE IF NOT EXISTS tasks (
-        task_text TEXT
+        task_text TEXT,
+        checked INTEGER,
+        end_date TEXT,
+        end_hour TEXT
     )
 ''')
+
 
 app = App(db_connection=conn)
 app.mainloop()
 
-# Don't forget to close the database connection when the program exits
 conn.close()
